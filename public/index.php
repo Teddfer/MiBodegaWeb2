5a07b737b77b49d1cb24197c9d6c487dd87e4534
Teddy Loc <clocquis@outlook.es>
2022-05-11 19:19:33 -0500

    Autocarga de clases PSR4 - OK

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 
 use Libs\Core;
 
-require_once "../libs/core.php";
-require_once "../app/helpers/helpers.php";
+require_once "../vendor/autoload.php";
+
+//require_once "../libs/core.php";
+//require_once "../app/helpers/helpers.php";
 
 $core = new Core();
